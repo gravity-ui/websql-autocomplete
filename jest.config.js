ef2fe1cfe2a23f892f4b5cbdeb92255ea1c6ec56
n-shkaruba <n-shkaruba@yandex-team.ru>
2023-05-30 17:46:41 +0200

    chore: remove useless lines from jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'vue'],
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json'],
   transform: {
     '^.+\\.(js|ts|jsx|tsx)$': 'babel-jest',
   },
@@ -10,18 +10,6 @@ module.exports = {
   moduleDirectories: ['node_modules', 'desktop/core/src/desktop/js'],
   modulePaths: ['desktop/core/src/desktop/js'],
   testMatch: ['<rootDir>/desktop/core/src/desktop/js/**/*.test.(js|jsx|ts|tsx)'],
-  testEnvironment: 'jest-environment-jsdom',
-  testURL: 'https://www.gethue.com/hue',
   setupFilesAfterEnv: ['<rootDir>/desktop/core/src/desktop/js/jest/jest.init.js'],
-  watchPathIgnorePatterns: ['<rootDir>/desktop/core/src/desktop/static'],
-  testPathIgnorePatterns: [
-    '<rootDir>/desktop/core/src/desktop/js/parse/sql/ksql/test',
-    '<rootDir>/desktop/core/src/desktop/js/parse/sql/elasticsearch/test',
-    '<rootDir>/desktop/core/src/desktop/js/parse/sql/phoenix/test',
-    '<rootDir>/desktop/core/src/desktop/js/parse/sql/druid/test',
-    '<rootDir>/desktop/core/src/desktop/js/parse/sql/presto/test',
-    '<rootDir>/desktop/core/src/desktop/js/parse/sql/calcite/test',
-    '<rootDir>/desktop/core/src/desktop/js/parse/sql/flink/test'
-  ],
-  collectCoverageFrom: ['<rootDir>/desktop/core/src/desktop/js/**/*.{js,jsx,vue}']
+  collectCoverageFrom: ['<rootDir>/desktop/core/src/desktop/js/**/*.{js,jsx}']
 };
