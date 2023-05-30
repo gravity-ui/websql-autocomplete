f585c30831d865fe93c97031c0bcd217f89a86e1
n-shkaruba <n-shkaruba@yandex-team.ru>
2023-05-30 14:30:25 +0200

    chore: rename sharedParserTest.ts to sqlTypedParseUtils.ts

--- desktop/core/src/desktop/js/parse/sql/generic/genericAutocompleteParser.test.ts
+++ desktop/core/src/desktop/js/parse/sql/generic/genericAutocompleteParser.test.ts
@@ -14,11 +14,25 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Please note that the code below is the modified code distributed on the terms, mentioned below.
+// The copyright for the changes belongs to YANDEX LLC.
+//
+// Copyright 2023 YANDEX LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License")
+// You may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software distributed under
+// the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific language governing permissions
+// and limitations under the License.
+
 import genericAutocompleteParser from './genericAutocompleteParser';
 import structure from './jison/structure.json';
 import { AutocompleteParser } from '../../types';
 import { extractTestCases, runTestCases } from '../testUtils';
-import { assertPartials, CommonParser } from '../sharedParserTests';
+import { assertPartials, CommonParser } from '../sqlTypedParseUtils';
 
 const jisonFolder = 'desktop/core/src/desktop/js/parse/sql/generic/jison';
 const groupedTestCases = extractTestCases(jisonFolder, structure.autocomplete);
