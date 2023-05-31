a16317ecf7ea3e918bb885f59a49f3417e13ef33
n-shkaruba <n-shkaruba@yandex-team.ru>
2023-05-31 15:25:17 +0200

    refactor: add src/parsing/index.ts

---
+++ src/parsing/index.ts
@@ -0,0 +1,62 @@
+import {genericAutocompleteParser} from './parsers/generic/genericAutocompleteParser';
+
+export abstract class Parser {
+    abstract parseSql(beforeCursor: string, afterCursor: string, debug?: boolean): ParseResult;
+}
+
+export interface ParseResult {
+    errors?: SyntaxError[];
+    suggestKeywords?: KeywordSuggestion[];
+    suggestTables?: {
+        prependFrom?: boolean;
+        prependQuestionMark?: boolean;
+    };
+    suggestColumns?: {
+        source: string;
+        tables: Table[];
+    };
+    suggestAggregateFunctions?: unknown;
+    suggestAnalyticFunctions?: unknown;
+    suggestColRefKeywords?: unknown;
+    suggestColumnAliases?: unknown;
+    suggestCommonTableExpressions?: unknown;
+    suggestDatabases?: unknown;
+    suggestFilters?: unknown;
+    suggestFunctions?: unknown;
+    suggestGroupBys?: unknown;
+}
+
+export interface KeywordSuggestion {
+    value: string;
+    weight: number;
+}
+
+export interface SyntaxError {
+    expected: string[];
+    line: number;
+    loc: Location;
+    recoverable: boolean;
+    ruleId: string;
+    text: string;
+    token: string;
+}
+
+export interface Location {
+    first_line: number;
+    first_column: number;
+    last_line: number;
+    last_column: number;
+}
+
+export interface Table {
+    identifierChain: IdentifierChainEntry[];
+}
+
+export interface IdentifierChainEntry {
+    name: string;
+}
+
+export function parseGenericSql(queryBeforeCursor: string, queryAfterCursor: string, debug: boolean): ParseResult {
+    let parser = genericAutocompleteParser as unknown as Parser;
+    return parser.parseSql(queryBeforeCursor, queryAfterCursor, debug) as ParseResult;
+}
