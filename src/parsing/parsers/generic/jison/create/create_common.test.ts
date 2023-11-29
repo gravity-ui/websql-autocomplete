f905a1a87671c699ed8ad22de76c1d50f80bb612
n-shkaruba <n-shkaruba@yandex-team.ru>
2023-11-29 13:43:50 +0100

    refactor: migrate generic/jison/create test to typescript

---
+++ src/parsing/parsers/generic/jison/create/create_common.test.ts
@@ -0,0 +1,29 @@
+import {
+    KeywordSuggestion,
+    parseGenericSql,
+} from '../../../../index';
+import {expect, test} from '@jest/globals';
+
+test('should suggest CREATE', () => {
+    const parseResult = parseGenericSql('', '');
+
+    expect(parseResult.errors).toBeUndefined();
+
+    const suggestion: KeywordSuggestion = { value: 'CREATE', weight: -1 };
+    expect(parseResult.suggestKeywords).toContainEqual(suggestion);
+})
+
+test('should suggest CREATE objects', () => {
+    const parseResult = parseGenericSql('CREATE ', '');
+
+    expect(parseResult.errors).toBeUndefined();
+
+    const suggestions: KeywordSuggestion[] = [
+        { value: 'DATABASE', weight: -1 },
+        { value: 'ROLE', weight: -1 },
+        { value: 'SCHEMA', weight: -1 },
+        { value: 'TABLE', weight: -1 },
+        { value: 'VIEW', weight: -1 },
+    ];
+    expect(parseResult.suggestKeywords).toEqual(expect.arrayContaining(suggestions))
+})
