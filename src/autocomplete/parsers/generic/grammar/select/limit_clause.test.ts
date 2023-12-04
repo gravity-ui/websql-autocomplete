957b7e1de619ad7a2e4ccc9e3459eeefe533cb69
n-shkaruba <n-shkaruba@yandex-team.ru>
2023-12-04 17:23:07 +0100

    refactor: migrate limit_clause test to typescript

---
+++ src/autocomplete/parsers/generic/grammar/select/limit_clause.test.ts
@@ -0,0 +1,29 @@
+import {expect, test} from '@jest/globals';
+
+import {KeywordSuggestion, parseGenericSql} from '../../../../index';
+
+test('should suggest values', () => {
+    const parseResult = parseGenericSql(
+        'SELECT COUNT(*) AS boo FROM testTable GROUP BY baa LIMIT ',
+        '',
+    );
+
+    expect(parseResult.errors).toBeUndefined();
+
+    const keywordSuggestions: KeywordSuggestion[] = [
+        {value: '10', weight: 10000},
+        {value: '100', weight: 10000},
+        {value: '1000', weight: 10000},
+        {value: '10000', weight: 10000},
+        {value: '5000', weight: 10000},
+    ];
+    expect(parseResult.suggestKeywords).toEqual(keywordSuggestions);
+});
+
+test('should allow to write offset with comma', () => {
+    const parseResult = parseGenericSql(
+        'SELECT COUNT(*) AS boo FROM testTable GROUP BY baa LIMIT 100, 100 ',
+        '',
+    );
+    expect(parseResult.errors).toBeUndefined();
+});
