5c98fb545cc1b8b50b9ad9e78e8e451228d13bbd
Nikita Shkaruba <n-shkaruba@yandex-team.ru>
2024-02-02 17:40:49 +0100

    Add separateQueryAndCursor tests, update documentation

--- src/tests/shared/lib.ts
+++ src/tests/shared/lib.ts
@@ -74,12 +74,17 @@ export function groupParseSqlWithoutCursor(
     return results;
 }
 
+// separateQueryAndCursor helps to calculate cursor position based on the pipe symbol `|`.
+//
+// It adapts human-readable input to c3 readable input, making tests very readable.
+// Otherwise, we'd need to manually count line and column positions ourselves, which is very inconvenient for writing tests.
 export function separateQueryAndCursor(query: string): [string, CursorPosition] {
     if (lineSeparatorRegex.test(query)) {
         throw new Error(`Newline characters not allowed, but present in query ${query}`);
     }
 
-    const [queryBeforeCursor, queryAfterCursor, ...excessQueries] = query.split('|');
+    const cursorSymbol = '|';
+    const [queryBeforeCursor, queryAfterCursor, ...excessQueries] = query.split(cursorSymbol);
 
     if (excessQueries.length > 0) {
         throw new Error(`Multiple cursors not allowed, but present in query ${query}`);
