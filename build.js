b5e192381e6282f374209556664c021ca642dd5f
itwillwork <edgarnurullin@yandex-team.ru>
2024-06-14 13:07:14 +0300

    build: lang separate build

--- build.js
+++ build.js
@@ -2,17 +2,27 @@ import {build} from 'esbuild';
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 (async () => {
+    const baseConfig = {
+        external: ['antlr4ng', 'antlr4-c3'],
+        bundle: true,
+        minify: true,
+        format: 'esm',
+        tsconfig: './tsconfig.build.json',
+    };
+
+    await build({
+        ...baseConfig,
+        entryPoints: [`src/autocomplete/index.ts`],
+        outfile: `dist/index.js`,
+    });
+
     const databases = ['clickhouse', 'mysql', 'postgresql', 'yql'];
 
     for (const database of databases) {
         await build({
+            ...baseConfig,
             entryPoints: [`src/autocomplete/databases/${database}/index.ts`],
-            external: ['antlr4ng', 'antlr4-c3'],
-            bundle: true,
-            minify: true,
-            format: 'esm',
             outfile: `dist/databases/${database}/index.js`,
-            tsconfig: './tsconfig.build.json',
         });
     }
 })();
