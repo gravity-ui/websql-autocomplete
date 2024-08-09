ad4fd3b66028094d688fea87ee1f531124d4f990
roberthovsepyan <rob.hovsepyan@gmail.com>
2024-08-09 14:35:19 +0300

    feat: redis autocomplete

--- src/autocomplete/databases/redis/redis-autocomplete.ts
+++ src/autocomplete/databases/redis/redis-autocomplete.ts
@@ -1,19 +1,78 @@
-import {ParseTree} from 'antlr4ng';
+import {ParseTree, TokenStream} from 'antlr4ng';
+import * as c3 from 'antlr4-c3';
 
 import {RedisLexer} from './generated/RedisLexer.js';
 import {RedisParser} from './generated/RedisParser.js';
+import {
+    AutocompleteResultBase,
+    CursorPosition,
+    ProcessVisitedRulesResult,
+} from '../../shared/autocomplete-types.js';
+import {RedisAutocompleteResult} from './index.js';
+import {isStartingToWriteRule} from '../../shared/cursor.js';
+import {shouldSuggestTemplates} from '../../shared/query.js';
 
 const tokenDictionary = {
     SPACE: RedisLexer.SPACE,
 };
 
 // These are keywords that we do not want to show in autocomplete
-const ignoredTokens = new Set([]);
+const ignoredTokens = new Set([
+    RedisParser.EOF,
+    RedisParser.NEWLINE,
+    RedisParser.SINGLE_QUOTE,
+    RedisParser.DOUBLE_QUOTE,
+    RedisParser.DECIMAL_LITERAL,
+    RedisParser.IDENTIFIER,
+]);
 
 const rulesToVisit = new Set([RedisParser.RULE_keyName]);
 
+function processVisitedRules(
+    rules: c3.CandidatesCollection['rules'],
+    cursorTokenIndex: number,
+): ProcessVisitedRulesResult<RedisAutocompleteResult> {
+    let suggestKeys = false;
+
+    for (const [ruleId, rule] of rules) {
+        if (!isStartingToWriteRule(cursorTokenIndex, rule)) {
+            continue;
+        }
+
+        switch (ruleId) {
+            case RedisParser.RULE_keyName: {
+                suggestKeys = true;
+                break;
+            }
+        }
+    }
+
+    return {
+        suggestKeys,
+    };
+}
+
 function getParseTree(parser: RedisParser): ParseTree {
     return parser.root();
+}
+
+function enrichAutocompleteResult(
+    baseResult: AutocompleteResultBase,
+    rules: c3.CandidatesCollection['rules'],
+    _tokenStream: TokenStream,
+    cursorTokenIndex: number,
+    cursor: CursorPosition,
+    query: string,
+): RedisAutocompleteResult {
+    const {suggestKeys} = processVisitedRules(rules, cursorTokenIndex);
+    const suggestTemplates = shouldSuggestTemplates(query, cursor);
+    const result: RedisAutocompleteResult = {
+        ...baseResult,
+        suggestKeys,
+        suggestTemplates,
+    };
+
+    return result;
 }
 
 export const redisAutocompleteData = {
@@ -23,4 +82,5 @@ export const redisAutocompleteData = {
     ignoredTokens,
     rulesToVisit,
     getParseTree,
+    enrichAutocompleteResult,
 };
--- src/autocomplete/databases/redis/tests/commands/commands.test.ts
+++ src/autocomplete/databases/redis/tests/commands/commands.test.ts
@@ -1,4 +1,5 @@
-import {parseRedisQueryWithoutCursor} from '../../index';
+import {KeywordSuggestion} from '../../../../shared';
+import {parseRedisQueryWithCursor, parseRedisQueryWithoutCursor} from '../../index';
 
 test('should not report errors on multiple (2) commands', () => {
     const autocompleteResult = parseRedisQueryWithoutCursor('GET test\nSET test test');
@@ -10,4 +11,40 @@ test('should not report errors on multiple (3) commands', () => {
     const autocompleteResult = parseRedisQueryWithoutCursor('GET test\nSET test test\nGET test');
 
     expect(autocompleteResult.errors).toHaveLength(0);
+});
+
+test('should suggest all commands', () => {
+    const autocompleteResult = parseRedisQueryWithCursor('|');
+
+    const keywordsSuggestion: KeywordSuggestion[] = [
+        {value: 'SET'},
+        {value: 'GET'},
+        {value: 'INCR'},
+        {value: 'DECR'},
+    ];
+    expect(autocompleteResult.suggestKeywords).toEqual(keywordsSuggestion);
+});
+
+test('should suggest all commands on second line', () => {
+    const autocompleteResult = parseRedisQueryWithCursor('GET test\n|');
+
+    const keywordsSuggestion: KeywordSuggestion[] = [
+        {value: 'SET'},
+        {value: 'GET'},
+        {value: 'INCR'},
+        {value: 'DECR'},
+    ];
+    expect(autocompleteResult.suggestKeywords).toEqual(keywordsSuggestion);
+});
+
+test('should suggest all commands on third line', () => {
+    const autocompleteResult = parseRedisQueryWithCursor('GET test\n\n|');
+
+    const keywordsSuggestion: KeywordSuggestion[] = [
+        {value: 'SET'},
+        {value: 'GET'},
+        {value: 'INCR'},
+        {value: 'DECR'},
+    ];
+    expect(autocompleteResult.suggestKeywords).toEqual(keywordsSuggestion);
 });
