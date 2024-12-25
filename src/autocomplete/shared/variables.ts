7304b400319d69bc4ecaf5b4a1618a869ae75eb7
Hellen <el-makarova@yandex-team.ru>
2024-12-25 17:14:31 +0300

    feat(YQL): improve variable symbol value types (#268)
    * feat(YQL): improve variable symbol value types
    
    * fix: review

--- src/autocomplete/shared/variables.ts
+++ src/autocomplete/shared/variables.ts
@@ -9,7 +9,7 @@ import {
     SymbolTableVisitor,
     VariableSuggestion,
 } from './autocomplete-types';
-import {getScope} from './symbol-table';
+import {VariableSymbol, getScope} from './symbol-table';
 import {computeTokenContext} from './compute-token-position';
 
 export function getVariableSuggestions<L extends LexerType, P extends ParserType>(
@@ -37,16 +37,16 @@ export function getVariableSuggestions<L extends LexerType, P extends ParserType
 
 function suggestVariables(symbolTable: c3.SymbolTable, context: ParseTree): VariableSuggestion[] {
     const scope = getScope(context, symbolTable);
-    let symbols: c3.VariableSymbol[] = [];
+    let symbols: VariableSymbol[] = [];
 
     // Local scope
     if (scope instanceof c3.ScopedSymbol) {
-        symbols = scope.getNestedSymbolsOfTypeSync(c3.VariableSymbol);
+        symbols = scope.getNestedSymbolsOfTypeSync(VariableSymbol);
 
         // Global scope
     } else if (symbolTable) {
         symbols = symbolTable
-            .getNestedSymbolsOfTypeSync(c3.VariableSymbol)
+            .getNestedSymbolsOfTypeSync(VariableSymbol)
             // If symbol's parent has context it means it is local scoped, so no need to suggest it in global scope
             .filter((symbol) => !symbol.parent?.context);
     }
