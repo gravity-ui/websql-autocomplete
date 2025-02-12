6d9817bbae417b2ad2147ef4b3ab830b95e61e4f
Nikita Shkaruba <n-shkaruba@yandex-team.ru>
2025-02-12 13:19:12 +0100

    feat: allow semicolon at the end of trino query (#288)

--- src/autocomplete/databases/trino/tests/multiple-statements.test.ts
+++ src/autocomplete/databases/trino/tests/multiple-statements.test.ts
@@ -1,12 +1,12 @@
 import {parseTrinoQueryWithoutCursor} from '../index';
 
 // TODO-TRINO: support multi-queries
-test.skip('should not report errors on multiple statements', () => {
+test('should report errors on multiple statements', () => {
     const autocompleteResult = parseTrinoQueryWithoutCursor(`
         SELECT * FROM test_table;
         SELECT * FROM test_table;
         SELECT * FROM test_table;
     `);
 
-    expect(autocompleteResult.errors).toHaveLength(0);
+    expect(autocompleteResult.errors).toHaveLength(1);
 });
---
+++ src/autocomplete/databases/trino/tests/semicolon-at-end.test.ts
@@ -0,0 +1,17 @@
+import {parseTrinoQueryWithoutCursor} from '../index';
+
+test('should not report errors on semicolons at the end', () => {
+    const autocompleteResult = parseTrinoQueryWithoutCursor(`
+        SELECT * FROM test_table;
+    `);
+
+    expect(autocompleteResult.errors).toHaveLength(0);
+});
+
+test('should allow queries without semicolon at the end', () => {
+    const autocompleteResult = parseTrinoQueryWithoutCursor(`
+        SELECT * FROM test_table
+    `);
+
+    expect(autocompleteResult.errors).toHaveLength(0);
+});
